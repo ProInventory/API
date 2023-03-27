5da2f6fad576e4aa423c5e9cc6ae7077daf9532a
Tharindu N <tharindu.nm@yahoo.com>
2023-03-27 20:33:59 +0530

    Mongoose
    Co-Authored-By: Sachin Dewthuru <101192491+RaZOr519@users.noreply.github.com>
    Co-Authored-By: L4Y3R <105732109+L4Y3R@users.noreply.github.com>
    Co-Authored-By: Dulan Jayawickrama <115356046+realDulanJay@users.noreply.github.com>
    Co-Authored-By: piumi00 <128916734+piumi00@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const config = require("config");
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(express.json());
 
+mongoose.connect(config.get("dbConnectionString")).then(() => {
+    console.log("Connected to MongoDB");
+});
+
 const server = app.listen(3000, () => {
-    winston.info("Server is running on port 3000");
+    console.log("Server is running on port 3000");
 });
 
 module.exports.server = server;
