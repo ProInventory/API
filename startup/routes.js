12a60b7b9fcaa4cb7e012dd56b36c2ad9cec0128
Tharindu N <tharindu.nm@yahoo.com>
2023-03-28 01:02:06 +0530

    Products
    Co-Authored-By: L4Y3R <105732109+L4Y3R@users.noreply.github.com>
    Co-Authored-By: Sachin Dewthuru <101192491+RaZOr519@users.noreply.github.com>
    Co-Authored-By: Piumi De Alwis <128916734+piumi00@users.noreply.github.com>
    Co-Authored-By: Dulan Jayawickrama <115356046+realDulanJay@users.noreply.github.com>

--- startup/routes.js
+++ startup/routes.js
@@ -1,9 +1,12 @@
 const express = require("express");
+
 const user = require("../routes/users");
 const branch = require("../routes/branches");
+const product = require("../routes/products");
 
 module.exports = function (app) {
     app.use(express.json());
     app.use("/api/users", user);
     app.use("/api/branches", branch);
+    app.use("/api/products", product);
 };
