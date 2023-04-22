b89906c722b2fcad6057dd7a311bebee55506e8f
Tharindu N <tharindu.nm@yahoo.com>
2023-04-22 14:31:14 +0530

    Set CORS
    Co-Authored-By: L4Y3R <105732109+L4Y3R@users.noreply.github.com>
    Co-Authored-By: Sachin Dewthuru <101192491+RaZOr519@users.noreply.github.com>
    Co-Authored-By: Piumi De Alwis <128916734+piumi00@users.noreply.github.com>
    Co-Authored-By: Dulan Jayawickrama <115356046+realDulanJay@users.noreply.github.com>

--- startup/routes.js
+++ startup/routes.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 
 const user = require("../routes/users");
 const branch = require("../routes/branches");
 const product = require("../routes/products");
 
 module.exports = function (app) {
-    app.use(express.json());
-    app.use("/api/users", user);
-    app.use("/api/branches", branch);
-    app.use("/api/products", product);
+	app.use(cors());
+	app.use(express.json());
+	app.use("/api/users", user);
+	app.use("/api/branches", branch);
+	app.use("/api/products", product);
 };
