f54e6452e04709cb4f09fe3a2a1d618096c95e9a
Tharindu N <tharindu.nm@yahoo.com>
2023-04-24 00:06:06 +0530

    change product model and change responses

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,70 +1,71 @@
 const { Product, validate } = require("../models/product");
 
 exports.getProducts = async (req, res) => {
-    const products = await Product.find().sort("name");
-    res.send(products);
+	const products = await Product.find().sort("name");
+	res.send(products);
 };
 
 exports.getProduct = async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (!product)
-        return res
-            .status(404)
-            .send("The product with the given ID was not found.");
-    res.send(product);
+	const product = await Product.findById(req.params.id);
+	if (!product)
+		return res
+			.status(404)
+			.send("The product with the given ID was not found.");
+	res.send(product);
 };
 
 exports.createProduct = async (req, res) => {
-    const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+	const { error } = validate(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
 
-    const product = new Product({
-        name: req.body.name,
-        category: req.body.category,
-        price: req.body.price,
-        quantity: req.body.quantity,
-    });
+	const product = new Product({
+		name: req.body.name,
+		category: req.body.category,
+		price: req.body.price,
+		quantity: req.body.quantity,
+		description: req.body.description,
+	});
 
-    await product.save();
-    res.send(product);
+	await product.save();
+	res.send(product);
 };
 
 exports.updateProduct = async (req, res) => {
-    if (
-        !req.body.name ||
-        !req.body.category ||
-        !req.body.price ||
-        !req.body.quantity
-    )
-        return res.status(400).send("Missing required fields.");
+	if (
+		!req.body.name ||
+		!req.body.category ||
+		!req.body.price ||
+		!req.body.quantity
+	)
+		return res.status(400).send("Missing required fields.");
 
-    const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+	const { error } = validate(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
 
-    const product = await Product.findByIdAndUpdate(
-        req.params.id,
-        {
-            name: req.body.name,
-            category: req.body.category,
-            price: req.body.price,
-            quantity: req.body.quantity,
-        },
-        { new: true }
-    );
+	const product = await Product.findByIdAndUpdate(
+		req.params.id,
+		{
+			name: req.body.name,
+			category: req.body.category,
+			price: req.body.price,
+			quantity: req.body.quantity,
+		},
+		{ new: true }
+	);
 
-    if (!product)
-        return res
-            .status(404)
-            .send("The product with the given ID was not found.");
+	if (!product)
+		return res
+			.status(404)
+			.send("The product with the given ID was not found.");
 
-    res.send(product);
+	res.send(product);
 };
 
 exports.deleteProduct = async (req, res) => {
-    const product = await Product.findByIdAndRemove(req.params.id);
-    if (!product)
-        return res
-            .status(404)
-            .send("The product with the given ID was not found.");
-    res.send(product);
+	const product = await Product.findByIdAndRemove(req.params.id);
+	if (!product)
+		return res
+			.status(404)
+			.send("The product with the given ID was not found.");
+	res.send(product);
 };
--- models/product.js
+++ models/product.js
@@ -2,42 +2,45 @@ const mongoose = require("mongoose");
 const Joi = require("joi");
 
 const productSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 50,
-        unique: true,
-    },
-    description: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 255,
-    },
-    price: {
-        type: Number,
-        required: true,
-    },
-    category: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 50,
-    },
+	name: {
+		type: String,
+		required: true,
+		minlength: 5,
+		maxlength: 50,
+		unique: true,
+	},
+	category: {
+		type: String,
+		required: true,
+		minlength: 5,
+		maxlength: 50,
+	},
+	price: {
+		type: Number,
+		required: true,
+	},
+	quantity: {
+		type: Number,
+		default: 0,
+	},
+	description: {
+		type: String,
+		maxlength: 255,
+	},
 });
 
 const Product = mongoose.model("Product", productSchema);
 
 function validateProduct(product) {
-    const schema = Joi.object({
-        name: Joi.string().min(5).max(50).required(),
-        description: Joi.string().min(5).max(255).required(),
-        price: Joi.number().required(),
-        category: Joi.string().min(5).max(50).required(),
-    });
+	const schema = Joi.object({
+		name: Joi.string().min(5).max(50).required(),
+		category: Joi.string().min(5).max(50).required(),
+		price: Joi.number().required(),
+		quantity: Joi.number(),
+		description: Joi.string().max(255),
+	});
 
-    return schema.validate(product);
+	return schema.validate(product);
 }
 
 module.exports.Product = Product;
